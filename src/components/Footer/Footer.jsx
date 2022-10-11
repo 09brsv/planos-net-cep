acd462c5a94311f79c3afea177bcd78d1150be02
Bruno Batista <104792685+09brsv@users.noreply.github.com>
2022-10-11 15:49:36 -0300

    Update Footer.jsx
    Changed position

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 import { GrReactjs } from "react-icons/gr";
 const Footer = () => {
   return (
-    <footer className="flex justify-around items-center rounded-md bg-cyan-100 bg-gradient-to-l from-cyan-300 backdrop-blur-md bg-transparent py-3 max-mob:static absolute bottom-0 z-40 w-full">
+    <footer className="flex justify-around items-center rounded-md bg-cyan-100 bg-gradient-to-l from-cyan-300 backdrop-blur-md bg-transparent py-3 fixed bottom-0 w-full">
       <h4>Feito por Bruno Batista</h4>
       <GrReactjs />
     </footer>
