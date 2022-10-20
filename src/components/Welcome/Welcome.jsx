bbd4839f3f36113517bf02e73776ecee9a847211
Bruno <brunoornelio@hotmail.com>
2022-10-19 21:22:12 -0300

    Aplicando responsividade no h1

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -2,7 +2,7 @@
 const Welcome = () => {
   return (
     <div className="flex flex-col px-3 max-mob:mt-20 mt-28">
-        <h1 className="text-5xl max-w-lg px-6 font-serif">Uma internet de qualidade para todos</h1>
+        <h1 className="text-3xl min-mob:text-5xl max-w-lg px-6 font-serif">Uma internet de qualidade para todos</h1>
         <p className="max-mob:text-sm text-md mt-28 mb-3 p-2 self-center">
         Insira o seu CEP para conhecer as ofertas disponíveis para a sua região !
         </p>
